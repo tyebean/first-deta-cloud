7b8273afaca1b6e6df56ad4547cccbb371123689
tyebean <flower.webster@gmail.com>
2023-01-13 21:54:00 -0500

    show all items

--- second_micro/index.js
+++ second_micro/index.js
@@ -2,9 +2,11 @@
 const express = require('express');
 const { Deta } = require("deta")
 const deta = Deta(process.env.PROJECT_KEY); // configure your Deta project
-const db = deta.Base("default-db")  // access your DB
 const app = express(); // instantiate express
 app.use(express.json()) // for parsing application/json bodies
+// * --------- access databases -------------
+const db = deta.Base("default-db")
+const blog_base = deta.Base("blog-base")
 
 
 // test
@@ -22,10 +24,22 @@ app.post('/users', async (req, res) => {
 
 // get all users
 app.get('/users', async (req, res) => {
-  // allUsers = db.fetch()
-  // alUsers.forEach(user => {
-  //   res.json(user)
-  // });
+  const users = await db.fetch()
+  if (users) {
+    res.status(200).json({
+      "data":users
+    })
+} else {
+    res.status(404).json({"message": "there are no users! add some!"});
+}
+});
+
+//create a blog
+app.post('/blogs', async (req, res) => {
+  const { title, subtitle, content } = req.body;
+  const toCreate = { title, subtitle, content };
+  const addBlog = await blog_base.put(toCreate); // put() will autogenerate a key for us
+  res.status(201).json(addBlog);
 });
 
 // get a specific user
