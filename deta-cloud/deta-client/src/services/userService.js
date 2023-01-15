9de369e71355764b31a5bd6ea62c9f1a948c7bed
tyebean <flower.webster@gmail.com>
2023-01-14 19:58:27 -0500

    add compontent folder

--- deta-cloud/deta-client/src/services/userService.js
+++ deta-cloud/deta-client/src/services/userService.js
@@ -1,2 +1,21 @@
 import { api } from './apiConfig.js'
 
+export const getAllUsers = async () => {
+  const res = await api.get('/users')
+  return res.data
+}
+
+export const getOneUser = async (user_key) => {
+  const res = await api.get(`/users/${user_key}`)
+  return res.data
+}
+
+export const updateUser = async (user_key, updateUser) => {
+  const res = await api.put(`/users/${user_key}`, {user: updateUser})
+  return res.data
+}
+
+export const deleteUser = async (user_key) => {
+  const res = await api.delete(`/users/${user_key}`)
+  return res.data
+}
