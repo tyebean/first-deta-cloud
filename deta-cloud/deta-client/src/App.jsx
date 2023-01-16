5a19bd225997b99df63ff46a5a9ee9b406036f4b
tyebean <flower.webster@gmail.com>
2023-01-15 23:48:12 -0500

    comment out unused code

--- deta-cloud/deta-client/src/App.jsx
+++ deta-cloud/deta-client/src/App.jsx
@@ -1,19 +1,18 @@
 import './App.css';
-import { useState } from "react";
-import { Routes, Route, useNavigate, Navigate } from "react-router-dom";
+// import { useState } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
 
 import NewUser from '../src/components/newUserForum/newUser.jsx'
 
 function App() {
 
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   return (
     <div className="App">
-    <Routes>
-    {/* forum */}
-    <Route path="/users" element={<NewUser />} />
-    </Routes>
+      <Routes>
+        <Route path="/users" element={<NewUser />} />
+      </Routes>
     </div>
   );
 }
