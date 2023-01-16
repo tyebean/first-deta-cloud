619745050bddcfd9a0c7c9994c922a465843cff1
tyebean <flower.webster@gmail.com>
2023-01-15 23:44:25 -0500

    fix import

--- deta-cloud/deta-client/src/components/newUserForum/newUser.jsx
+++ deta-cloud/deta-client/src/components/newUserForum/newUser.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 // import { useNavigate } from 'react-router-dom'
-import '../newUserForm.css'
+import '../newUserForum/newUser.css'
 
 const NewUser = props => {
 
