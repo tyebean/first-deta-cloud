8bfb4358835491c78455b99951fafe39291657b3
tyebean <flower.webster@gmail.com>
2023-01-24 12:15:48 -0500

    stub up alll users box

---
+++ deta-cloud/deta-client/src/components/allUsersBox/allUsersBox.jsx
@@ -0,0 +1,13 @@
+import * as userService from './services/userService.js'
+
+const allUsersBox = props => {
+
+  return (
+    <>
+      <h1>These are all the users</h1>
+    </>
+
+  )
+}
+  
+export default allUsersBox
